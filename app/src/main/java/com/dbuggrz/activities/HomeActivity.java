b33333684826454257fd972e65ba2b14da78748b
miketheprogrammer1 <miketheprogrammer@gmail.com>
2016-04-30 10:51:58 -0400

    Linking the home activity to the monitor activity

--- app/src/main/java/com/dbuggrz/activities/HomeActivity.java
+++ app/src/main/java/com/dbuggrz/activities/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.dbuggrz.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.altbeacon.beaconreference.MonitoringActivity;
 import org.altbeacon.beaconreference.R;
 
 import java.util.ArrayList;
@@ -15,6 +20,8 @@ public class HomeActivity extends Activity {
 
     private ListView mainListView ;
     private ArrayAdapter<String> listAdapter ;
+
+    private static final String TAG = HomeActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +43,30 @@ public class HomeActivity extends Activity {
         // Set the ArrayAdapter as the ListView's adapter.
         mainListView.setAdapter( listAdapter );
 
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String listItemText = listAdapter.getItem(position);
+                // Executed in an Activity, so 'this' is the Context
+                // The fileUrl is a string URL, such as "http://www.example.com/image.png"
+                if ("Conference Rooms".equalsIgnoreCase(listItemText)) {
+                    Intent monitoringIntent = new Intent(view.getContext(), MonitoringActivity.class);
+                    startActivity(monitoringIntent);
+                }
+            }
+        });
 
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "RESUMING THE HOME ACTIVITY");
+    }
 
+    public void onConferenceRoomsClicked(View view) {
+        Log.d(TAG, "onRangingClicked - we clicked a thing.");
+        Intent myIntent = new Intent(this, MonitoringActivity.class);
+        this.startActivity(myIntent);
     }
 }
