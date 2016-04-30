b6bb37049a839909465cd719cf4add23b0998409
jasonsit <sit.jason@gmail.com>
2016-04-30 11:21:57 -0400

    Adding conf room detail activity

---
+++ app/src/main/java/com/dbuggrz/activities/ConfDetailActivity.java
@@ -0,0 +1,13 @@
+package com.dbuggrz.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf_detail);
+    }
+}
